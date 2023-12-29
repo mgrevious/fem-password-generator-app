9e4103ef49300e7df419bcc8f0f169d915ba525f
Monica Grevious <mag.amtrust@gmail.com>
2023-12-29 00:46:53 -0500

    Fixed container sizing for tablet.

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import "./App.css";
 function App() {
   return (
     <AppProvider>
-      <main className="container w-full lg:w-[540px] mx-auto min-h-screen">
+      <main className="container w-full sm:max-w-[540px] mx-auto min-h-screen">
         <div className="flex flex-col justify-center items-center mx-5 min-h-screen">
           <h1 className="text-lg text-light-gray mb-4">Password Generator</h1>
           <section className="mb-4 bg-dark-gray p-4 lg:px-8 lg:py-7 w-full">
